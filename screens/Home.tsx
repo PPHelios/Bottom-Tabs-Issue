796bcd7a47a3ca1a40f92b677ffdbe784f2c9ff0
PPHelios <pphelios1@gmail.com>
2024-04-15 17:28:10 +0200

    BottomNavigationIssue

---
+++ screens/Home.tsx
@@ -0,0 +1,13 @@
+import { View, Text, TextInput } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1,width:"90%"}}/>
+    </View>
+  )
+}
+
+export default Home
