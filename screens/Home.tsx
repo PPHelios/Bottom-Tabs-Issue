401fa9e293b4e02390327147cd03cd191401b647
PPHelios <pphelios1@gmail.com>
2024-04-16 10:51:35 +0200

    react-native-avoid-softinput

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,12 +1,23 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, ScrollView } from 'react-native'
 import React from 'react'
-
+import {AvoidSoftInput} from 'react-native-avoid-softinput';
+import {useFocusEffect} from '@react-navigation/native';
 const Home = () => {
+  const onFocusEffect = React.useCallback(() => {
+    AvoidSoftInput.setShouldMimicIOSBehavior(true);
+    AvoidSoftInput.setEnabled(true);
+    return () => {
+      AvoidSoftInput.setEnabled(false);
+      AvoidSoftInput.setShouldMimicIOSBehavior(false);
+    };
+  }, []);
+
+  useFocusEffect(onFocusEffect);
   return (
-    <View>
+    <ScrollView>
       <Text>Home</Text>
       <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1,width:"90%"}}/>
-    </View>
+    </ScrollView>
   )
 }
 
