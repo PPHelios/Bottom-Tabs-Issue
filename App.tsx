dce53576273ebf6dad06f121ee55b4cd8c2fbc77
PPHelios <pphelios1@gmail.com>
2024-04-24 19:55:31 +0200

    pos absolute

--- App.tsx
+++ App.tsx
@@ -6,43 +6,46 @@
  */
 
 import React, {useEffect, useState} from 'react';
-import {Dimensions, StatusBar, View, useWindowDimensions} from 'react-native';
+// import {Dimensions, StatusBar, View, useWindowDimensions} from 'react-native';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 
 import Home from './screens/Home';
 import Settings from './screens/Settings';
+import { View } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
-  const {height, width} = useWindowDimensions();
-  const [screenHeight, setScreenHeight] = useState(height);
-  useEffect(() => {
-    const extras =
-      Dimensions.get('screen').height - Dimensions.get('window').height;
-    let newHeight =
-      Dimensions.get('screen').height - extras - (StatusBar.currentHeight || 0);
-    // console.log({newHeight});
-    if (Dimensions.get('screen').height !== Dimensions.get('window').height) {
-      newHeight = Dimensions.get('screen').height - extras;
-    }
-    if (newHeight !== screenHeight) {
-      setScreenHeight(newHeight);
-    }
-  }, [width, height]);
+  // const {height, width} = useWindowDimensions();
+  // const [screenHeight, setScreenHeight] = useState(height);
+  // useEffect(() => {
+  //   const extras =
+  //     Dimensions.get('screen').height - Dimensions.get('window').height;
+  //   let newHeight =
+  //     Dimensions.get('screen').height - extras - (StatusBar.currentHeight || 0);
+  //   // console.log({newHeight});
+  //   if (Dimensions.get('screen').height !== Dimensions.get('window').height) {
+  //     newHeight = Dimensions.get('screen').height - extras;
+  //   }
+  //   if (newHeight !== screenHeight) {
+  //     setScreenHeight(newHeight);
+  //   }
+  // }, [width, height]);
   return (
     <NavigationContainer>
       {/* <View style={{height: screenHeight, width}}> */}
+      <View style={{flex:1,position:"relative"}}>
         <Tab.Navigator
           screenOptions={{
             tabBarHideOnKeyboard: true,
-            // tabBarStyle: { position: 'absolute', borderRadius: 30,marginBottom:20}
+            tabBarStyle: { position: 'absolute', bottom:0}
           }}>
           <Tab.Screen name="Home" component={Home} />
           <Tab.Screen name="Settings" component={Settings} />
         </Tab.Navigator>
+        </View>
       {/* </View> */}
     </NavigationContainer>
   );
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,22 +1,24 @@
 import { View, Text, TextInput, ScrollView } from 'react-native'
 import React from 'react'
-import {AvoidSoftInput} from 'react-native-avoid-softinput';
-import {useFocusEffect} from '@react-navigation/native';
+// import {AvoidSoftInput} from 'react-native-avoid-softinput';
+// import {useFocusEffect} from '@react-navigation/native';
 const Home = () => {
-  const onFocusEffect = React.useCallback(() => {
-    AvoidSoftInput.setShouldMimicIOSBehavior(true);
-    AvoidSoftInput.setEnabled(true);
-    return () => {
-      AvoidSoftInput.setEnabled(false);
-      AvoidSoftInput.setShouldMimicIOSBehavior(false);
-    };
-  }, []);
+  // const onFocusEffect = React.useCallback(() => {
+  //   AvoidSoftInput.setShouldMimicIOSBehavior(true);
+  //   AvoidSoftInput.setEnabled(true);
+  //   return () => {
+  //     AvoidSoftInput.setEnabled(false);
+  //     AvoidSoftInput.setShouldMimicIOSBehavior(false);
+  //   };
+  // }, []);
 
-  useFocusEffect(onFocusEffect);
+  // useFocusEffect(onFocusEffect);
   return (
     <ScrollView>
+      <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
       <Text>Home</Text>
-      <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1,width:"90%"}}/>
+      <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1,width:"90%"}}/>        
+      </View>
     </ScrollView>
   )
 }
